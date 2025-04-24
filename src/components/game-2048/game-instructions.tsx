5577ef5b1ccce8debe10fbde346a69cf1a44065f
Yu Deng <deng1193291298@icloud.com>
2025-04-24 11:51:15 +0800

    Add safe-area

--- src/components/game-2048/game-instructions.tsx
+++ src/components/game-2048/game-instructions.tsx
@@ -4,7 +4,7 @@ import { FC } from 'react';
 export const GameInstructions: FC = () => {
   return (
     <div className={cn('mt-6 px-6 text-center text-sm text-gray-600')}>
-      Use arrow keys or WASD to move tiles; on mobile, swipe to move. <br></br> Tiles with the same number merge when they touch.
+      Use Arrow Keys, WASD or Swipe to move tiles. <br></br> Tiles with the same number merge when they touch.
     </div>
   );
 };
